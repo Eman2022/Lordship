4b09d3a28cdcdd3be6d0a798f8c0df2a8669ce60
Erich Lamontagne <eman2881@outlook.com>
2026-05-14 13:16:03 -0700

    Initial project setup

---
+++ src/main/java/io/github/lordship/LordshipApplication.java
@@ -0,0 +1,13 @@
+package io.github.lordship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordshipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LordshipApplication.class, args);
+    }
+
+}
